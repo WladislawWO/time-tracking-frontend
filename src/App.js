186bac948ea669b792fcab00baa23ed95aee04db
Vladislav <wolodislaw@gmail.com>
2022-06-03 22:15:26 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import st from './App.module.scss';
 
 function App() {
+  const timeTracking = [1,1,1,1];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid className={st.app}>
+      <Grid className={st.trackingContainer}>
+        {timeTracking.map(() => (
+          <Card className={st.card}>
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                1
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">View Details</Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Grid>
+    </Grid>
   );
 }
 
