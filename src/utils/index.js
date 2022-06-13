2a5363924efbfebfd5d20ae96ae94996c55b5fef
WladislawWO <wolodislaw@gmail.com>
2022-06-13 15:51:20 +0300

    fixed get time util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 export const getTime = (time) => {
   const hours = Math.floor(time / 60);
   const minutes = time % 60;
-  let res = `${hours || ''}${hours && minutes ? '.' : ''}${minutes}`;
+  let res = `${((hours === 0 && minutes) || (!hours && !minutes)) ? '' : hours}${hours && minutes ? '.' : ''}${minutes === 0 && hours ? '' : minutes}`;
   if (minutes && !hours) {
     res += 'm';
   } else if (hours) {
